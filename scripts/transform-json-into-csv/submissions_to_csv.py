58cf2ce4d9ab218e77917e49f3db5947bceef5dc
Guilherme Zanetti <guilherme.t.zanetti@gmail.com>
2025-11-10 21:53:31 -0300

    finalizado testes A até J

--- scripts/transform-json-into-csv/submissions_to_csv.py
+++ scripts/transform-json-into-csv/submissions_to_csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 
-caminho_arquivo = 'c:/projects/tcc_api/scripts/transform-json-into-csv/B'
+caminho_arquivo = 'c:/projects/tcc_api/scripts/transform-json-into-csv/J'
 # Nome do seu arquivo JSON de entrada
 json_filename = f'{caminho_arquivo}/response.json'
 # Nome do arquivo CSV de saída
